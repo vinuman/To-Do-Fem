f94ce97ca555041610a211facb51e2547de809a1
Vinu Cyril <vinucyril@gmail.com>
2023-08-16 22:49:37 +0530

    UI change

--- src/features/todo/TodoSlice.js
+++ src/features/todo/TodoSlice.js
@@ -3,13 +3,13 @@ import { nanoid } from "@reduxjs/toolkit";
 
 const initialState = [
   { id: 1, title: "Jog around park 3x", active: true },
-  { id: 2, title: "Complete online JavaScript course", active: false },
+  { id: 2, title: "Complete JavaScript", active: false },
   { id: 3, title: "10 minutes meditation", active: true },
   { id: 4, title: "Read for 1 hour", active: true },
   { id: 5, title: "Pick up groceries", active: true },
   {
     id: 6,
-    title: "Complete Todo App on Frontend Mentor",
+    title: "Complete Todo App",
     active: true,
   },
 ];
